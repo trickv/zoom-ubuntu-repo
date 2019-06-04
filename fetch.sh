f1a5df7e3706b28af7e242f6e5dafe1630d86064
Patrick van Staveren <trick@vanstaveren.us>
2019-06-04 16:11:25 -0500

    fix fetch script variables

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-set -u
-
 version="latest"
 if [ "$1" != "" ]; then
     version=$1
 fi
 
+set -u
 set -e
 
 arch=amd64
@@ -15,5 +14,6 @@ rm -rf DEBIAN $deb_name
 wget -q https://zoom.us/client/$version/$deb_name
 dpkg -e $deb_name
 version=$(cat DEBIAN/control | grep ^Version | cut -d\  -f2)
+echo "Fetched zoom version $version"
 mv $deb_name zoom_${version}_$arch.deb
 rm -rf DEBIAN $deb_name
