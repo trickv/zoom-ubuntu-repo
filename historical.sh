9a37047190bcb6186a16ad1eeb6e28b8644863e0
Patrick van Staveren <trick@vanstaveren.us>
2019-06-04 16:25:28 -0500

    add slightly older version of 2.8 that i was on

--- historical.sh
+++ historical.sh
@@ -9,6 +9,7 @@ versions+=("2.4.129780.0915")
 versions+=("2.6.146750.1204")
 versions+=("2.6.149990.1216")
 versions+=("2.7.162522.0121")
+versions+=("2.8.183302.0415")
 versions+=("2.8.222599.0519")
 
 for version in ${versions[@]}; do
