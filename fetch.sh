cc005f0e4b0ee15ea25dd61a49c8d431318c9c1c
Patrick van Staveren <trick@vanstaveren.us>
2019-06-04 16:06:32 -0500

    modify latest.sh to fetch either the latest version or a specified historical version, and rename it appropriately

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 set -u
+
+version="latest"
+if [ "$1" != "" ]; then
+    version=$1
+fi
+
 set -e
 
 arch=amd64
 deb_name=zoom_$arch.deb
 rm -rf DEBIAN $deb_name
-wget -q https://zoom.us/client/latest/$deb_name
+wget -q https://zoom.us/client/$version/$deb_name
 dpkg -e $deb_name
 version=$(cat DEBIAN/control | grep ^Version | cut -d\  -f2)
 mv $deb_name zoom_${version}_$arch.deb
---
+++ historical.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+versions=()
+
+versions+=("2.1.103753.0521")
+versions+=("2.3.128305.0716")
+versions+=("2.4.121350.0816")
+versions+=("2.4.129780.0915")
+versions+=("2.6.146750.1204")
+versions+=("2.6.149990.1216")
+versions+=("2.7.162522.0121")
+versions+=("2.8.222599.0519")
+
+for version in $versions; do
+    echo "Fetching zoom $version"
+    ./latest.sh $version
+done
