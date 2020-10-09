aff4e27dc8fb39bb077ccf5a232437eda8845c13
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2020-10-09 10:19:35 +0100

    go crazy with quoting to defend against rm -r / etc
    Spaces or other unintended goo could be accidentally injected into
    filenames by broken utilities, so take a little extra care.

--- fetch.sh
+++ fetch.sh
@@ -8,21 +8,21 @@ fi
 set -u
 set -e
 
-arch=$(dpkg --print-architecture)
+arch="$(dpkg --print-architecture)"
 echo "arch: ${arch}"
-deb_name=zoom_$arch.deb
-rm -f $deb_name
-wget -q https://zoom.us/client/$version/$deb_name
+deb_name="zoom_$arch.deb"
+rm -f "$deb_name"
+wget -q "https://zoom.us/client/$version/$deb_name"
 
-version=$(dpkg -f ${deb_name} Version)
+version="$(dpkg -f "${deb_name}" Version)"
 echo "Latest zoom version is $version"
-target=zoom_${version}_$arch.deb
-if [ -e $target ]; then
+target="zoom_${version}_$arch.deb"
+if [ -e "$target" ]; then
     echo "$target already exists. No action."
 else
     echo "Adding $target to repository."
-    mv $deb_name $target
+    mv "$deb_name" "$target"
     ./build-repo.sh
 fi
 
-rm -f $deb_name
+rm -f "$deb_name"
