9f284b153e91197d4fe78a1dda295ce58392188e
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2020-10-29 11:51:49 +0000

    download asynchronously, aborting if current version already downloaded
    Snuffle through the file as it downloads until version is found, then
    abort download if that version's already been fetched. Below is a
    transcript showing this in action. Nearly five seconds of savings!!!
    
    $ time -p ./fetch.sh
    arch: amd64
    tail: warning: --retry only effective for the initial open
    info: downloading zoom package has version 5.4.53350.1027
    info: waiting for download to complete.
    info: moving into place.
    info: rebuilding repo.
    dpkg-scanpackages: info: Wrote 1 entries to output Packages file.
    real 5.43
    
    $ time -p ./fetch.sh
    arch: amd64
    tail: warning: --retry only effective for the initial open
    info: downloading zoom package has version 5.4.53350.1027
    info: aborting download, zoom_5.4.53350.1027_amd64.deb already exists.
    real 0.64

--- fetch.sh
+++ fetch.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 
-version="latest"
-if [ "$1" != "" ]; then
-    version=$1
-fi
-
+set -f
 set -u
 set -e
 
-arch="$(dpkg --print-architecture)"
-echo "arch: ${arch}"
-deb_name="zoom_$arch.deb"
-rm -f "$deb_name"
-wget -q "https://zoom.us/client/$version/$deb_name"
-
-version="$(dpkg -f "${deb_name}" Version)"
-echo "Latest zoom version is $version"
-target="zoom_${version}_$arch.deb"
-if [ -e "$target" ]; then
-    echo "$target already exists. No action."
+if [ $# = 0 ]; then
+    version="latest"
+elif [ $# = 1 -a -n "$1" ]; then
+    version="$1"
 else
-    echo "Adding $target to repository."
-    mv "$deb_name" "$target"
-    ./build-repo.sh
+    echo "Usage: $0 [version]"
+    echo "usage error: incorrect number or arguments or empty argument"
+    exit 1
 fi
 
-rm -f "$deb_name"
+arch="$(dpkg --print-architecture)"
+echo "arch: ${arch}"
+deb_name="zoom_${arch}.deb"
+rm -f "${deb_name}"
+
+# Here we download the file, but as it downloads we examine its header
+# to grovel out the version, and if we already have that version the
+# download is terminated early. The trick is to put the downloading
+# process in the background, and then trickle the file being
+# downloaded into a scanning process until enough has been seen to
+# extract the version. And then decide what to do.
+
+# Background this process:
+wget -q --output-document=- "https://zoom.us/client/${version}/${deb_name}" > "${deb_name}" &
+
+# Snuffle through downloading file to extract version:
+v="$(dpkg --field <(tail --retry --follow --bytes=+0 "${deb_name}") Version)"
+echo "info: downloading zoom package has version ${v}"
+
+target="zoom_${v}_${arch}.deb"
+
+if [ -e "${target}" ]; then
+    echo "info: aborting download, ${target} already exists."
+    kill $!
+    wait
+    rm -f "${deb_name}"
+else
+    echo "info: waiting for download to complete."
+    wait
+    echo "info: moving into place."
+    mv "${deb_name}" "${target}"
+    echo "info: rebuilding repo."
+    ./build-repo.sh
+fi
