4ceb1618f2d0d5520f1063bcf15d9cb87b05568d
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2020-11-02 13:13:05 +0000

    do not freeze indefinitely when the download fails

--- fetch.sh
+++ fetch.sh
@@ -27,10 +27,12 @@ rm -f "${deb_name}"
 # extract the version. And then decide what to do.
 
 # Background this process:
-wget -q --output-document=- "https://zoom.us/client/${version}/${deb_name}" > "${deb_name}" &
+wget --quiet --output-document=- "https://zoom.us/client/${version}/${deb_name}" > "${deb_name}" &
+
+downloadpid=$!
 
 # Snuffle through downloading file to extract version:
-v="$(dpkg --field <(tail --retry --follow --bytes=+0 "${deb_name}") Version)"
+v="$(dpkg --field <(tail --retry --follow --pid=${downloadpid} --bytes=+0 "${deb_name}") Version)"
 echo "info: downloading zoom package has version ${v}"
 
 target="zoom_${v}_${arch}.deb"
@@ -38,11 +40,11 @@ target="zoom_${v}_${arch}.deb"
 if [ -e "${target}" ]; then
     echo "info: aborting download, ${target} already exists."
     kill $!
-    wait
+    wait -f
     rm -f "${deb_name}"
 else
     echo "info: waiting for download to complete."
-    wait
+    wait -f
     echo "info: moving into place."
     mv "${deb_name}" "${target}"
     echo "info: rebuilding repo."
