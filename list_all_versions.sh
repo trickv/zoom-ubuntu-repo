951b8c0d7e4b7d81729e51a96ae8f4ab355ec289
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2020-12-13 14:18:02 +0000

    close race condition in list_all_versions.sh, and some light preening

--- list_all_versions.sh
+++ list_all_versions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 
 ZOOM_LINUX_URL="https://support.zoom.us/hc/en-us/articles/205759689-New-Updates-for-Linux"
 
-TMP=$(mktemp /tmp/zoom-versions-XXXXXXXX)
-wget -q -O "$TMP" "$ZOOM_LINUX_URL"
+TMP=$(mktemp --tmpdir zoom-versions-XXXXXXXX)
+wget --quiet --output-document=- "$ZOOM_LINUX_URL" >> "$TMP"
 
-cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=<br /></strong>)'
-cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=</strong>)'
+grep -oP '(?<=<strong>)[0-9\.]*(?=<br /></strong>)'		< "$TMP" || true
+grep -oP '(?<=<strong>)[0-9\.]*(?=</strong>)'			< "$TMP" || true
 
-rm "$TMP"
+rm -f "$TMP"
