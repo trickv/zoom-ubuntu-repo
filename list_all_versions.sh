16182637bbf4b55809fb74afdb4fa21a7c281daa
Marcel <marcel+it+gitkraken@linux-ng.de>
2021-10-04 10:56:19 +0200

    New

--- list_all_versions.sh
+++ list_all_versions.sh
@@ -8,5 +8,7 @@ wget -q -O "$TMP" "$ZOOM_LINUX_URL"
 cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=<br /></strong>)'
 cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=</strong>)'
 cat "$TMP"  | grep -oP '[0-9]+\.[0-9]+\.+[0-9]+\ \([0-9]+\.[0-9]+\)' | sed -e s+'\.[0-9]* ('+'.'+ -e s+')'+''+
+# New format: "5.8.0 (16)"
+cat "$TMP"  | grep -oP '[0-9]+\.[0-9]+\.+[0-9]+\ \([0-9]+\)' | sed -e s+' ('+'.'+ -e s+')'+''+
 
 rm "$TMP"
