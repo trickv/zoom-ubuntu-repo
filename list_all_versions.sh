5c97f8ad993857ec0afa01cdd41ea83d81d2d298
Marcel <marcel+it+gitkraken@linux-ng.de>
2021-10-14 15:07:53 +0200

    Now with cloudflare support (w3m instead of wget)

--- list_all_versions.sh
+++ list_all_versions.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ZOOM_LINUX_URL="https://support.zoom.us/hc/en-us/articles/205759689-New-Updates-for-Linux"
+#ZOOM_LINUX_URL="https://support.zoom.us/hc/en-us/articles/205759689-New-Updates-for-Linux"
+ZOOM_LINUX_URL="https://support.zoom.us/hc/en-us/articles/205759689"
 
 TMP=$(mktemp /tmp/zoom-versions-XXXXXXXX)
-wget -q -O "$TMP" "$ZOOM_LINUX_URL"
+#wget -q -O "$TMP" "$ZOOM_LINUX_URL"
+# Zoom web page is now protected by cloudflare
+# This requires a more clever download util (usually called browser)
+w3m -dump_source "$ZOOM_LINUX_URL" | gzip -cd > "$TMP"
 
 cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=<br /></strong>)'
 cat "$TMP"  | grep -oP '(?<=<strong>)[0-9\.]*(?=</strong>)'
